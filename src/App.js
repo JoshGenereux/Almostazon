cf0e02a4a93749cdda60b872f9452cd94571ceea
JoshGenereux <josh.genereux13@gmail.com>
2022-03-17 14:44:05 -0600

    navbar styled

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Header from "./components/header/Header";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Navbar />
     </div>
   );
 }
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,18 +1,21 @@
 import React from "react";
 import './navbar.css';
+import {faUser, faCartShopping, faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const Navbar = () =>{
 
   return (
     <div className='nav-container'>
       <nav className='nav'>
+        <h1>Almostazon</h1>
         <button>Home</button>
         <form className='search'>
           <input className='searchbar' type='text' placeholder='search...'/>
-          <button className='search-btn'>Search</button>
+          <button className='search-btn'>Search{<FontAwesomeIcon icon={faMagnifyingGlass}/>}</button>
         </form>
-        <button>Account</button>
-        <button>Cart</button>
+        <button>Account {<FontAwesomeIcon icon={faUser}/>}</button>
+        <button>Cart {<FontAwesomeIcon icon={faCartShopping}/>}</button>
       </nav>
     </div>
   )
